d117d5b243e0dbbf9c0184a1c3f938cc3540090c
xianjiezh <904292766@qq.com>
2017-11-16 23:35:03 +0800

    add draw line function

--- main.js
+++ main.js
@@ -1,29 +1,63 @@
+
 var canvas = document.getElementById('id-canvas')
 
 var ctx = canvas.getContext('2d')
 
+var paiting = false
 
+var lastPoint = {
+    x: undefined,
+    y: undefined
+}
 
-function drawCircle(x,y) {
+var newPoint = {
+    x: undefined,
+    y: undefined
+}
+
+function drawCircle(x, y, radius) {
     ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.fill();
 } 
-
-
-canvas.onmousemove = function(move) {
-    // console.log(move)
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1,y1)
+    ctx.lineTo(x2,y2)
+    ctx.lineWidth = 10
+    ctx.closePath()
+    ctx.stroke()
 }
 
 canvas.onmousedown = function(down) {
-    console.log(down)
+    paiting = true
     var x = down.clientX,
         y = down.clientY
-    drawCircle(x,y)
+    lastPoint = {
+        x:x,
+        y:y
+    }
+    drawCircle(x, y, 10)
         
 }
 
+canvas.onmousemove = function(move) {
+    // console.log(move)
+    var x = move.clientX,
+    y = move.clientY
+    newPoint = {
+        x:x,
+        y:y
+    }
+    if(paiting == true){
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)        
+    }
+    lastPoint = newPoint  //这句话好牛逼
+
+}
+
 canvas.onmouseup = function(up) {
+    paiting = false
     console.log(up)
     var x = up.clientX,
         y = up.clientY

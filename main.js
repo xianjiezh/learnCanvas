be4511953e1ce80a9a1f8562c5d63ce248204f95
xianjiezh <904292766@qq.com>
2017-11-18 21:06:01 +0800

    add eraser

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ var ctx = canvas.getContext('2d')
 resize()
 function resize() {
     var canvasWidth = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth
-    
+        || document.documentElement.clientWidth
+        || document.body.clientWidth
+
     var canvasHeight = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight
     canvas.width = canvasWidth
     canvas.height = canvasHeight
 }
-window.onresize = function() {
+window.onresize = function () {
     resize()
 }
 
 
-var paiting = false
+var using = false
 
 var lastPoint = {
     x: undefined,
@@ -36,45 +36,58 @@ function drawCircle(x, y, radius) {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.fill();
-} 
+}
 function drawLine(x1, y1, x2, y2, width) {
     ctx.beginPath()
-    ctx.moveTo(x1,y1)
-    ctx.lineTo(x2,y2)
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
     ctx.lineWidth = width
     ctx.closePath()
     ctx.stroke()
 }
 
-canvas.onmousedown = function(down) {
-    paiting = true
+var eraserEnabled = false
+var actions = document.getElementById('eraser')
+actions.onclick = function () {
+    eraserEnabled = !eraserEnabled
+}
+
+canvas.onmousedown = function (down) {
+
+    using = true
     var x = down.clientX,
         y = down.clientY
-    lastPoint = {
-        x:x,
-        y:y
+    if (eraserEnabled) {
+        ctx.clearRect(x - 3, y - 3, 6, 6)
+    } else {
+        drawCircle(x, y, 2.5)
     }
-    drawCircle(x, y, 2.5)
-        
-}
 
-canvas.onmousemove = function(move) {
+}
+canvas.onmousemove = function (move) {
     // console.log(move)
     var x = move.clientX,
-    y = move.clientY
+        y = move.clientY
     newPoint = {
-        x:x,
-        y:y
+        x: x,
+        y: y
     }
-    if(paiting == true){
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, 5)        
-    }
-    lastPoint = newPoint  //这句话好牛逼
+
+        if (using) {
+            if(eraserEnabled){
+                ctx.clearRect(x -3, y -3, 6, 6)
+            } else {
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, 5) 
+            }
+        }
+        lastPoint = newPoint //这句话好牛逼
+    
+
 
 }
+canvas.onmouseup = function (up) {
+    using = false
 
-canvas.onmouseup = function(up) {
-    paiting = false
     var x = up.clientX,
         y = up.clientY
 }
